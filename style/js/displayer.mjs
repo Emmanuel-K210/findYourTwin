e5324bb773abb79677c400a556f3da0018046544
emmanuel-K210 <coderemmane210@gmail.com>
2025-09-28 18:43:10 +0000

    first commit

---
+++ style/js/displayer.mjs
@@ -0,0 +1,131 @@
+class FundBothGame {
+    #container = document.createElement("div");
+    #image_link = "../ressources/images/image_"
+    #selected = [];
+    #bothFund = [];
+    #isWinner = false;
+    #score = 0;
+    #click = 0;
+    #start = false
+
+    #create() {
+        for (let i = 1; i <= 8; i++) {
+            const url_image = this.#image_link + i + ".png";
+            const img = new Image();
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+            canvas.width = 100;
+            canvas.height = 100;
+            canvas.dataset.id = i;
+
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.appendChild(canvas);
+
+            const cover = document.createElement("div");
+            cover.classList.add("cover");
+            card.appendChild(cover);
+
+            cover.addEventListener("click", () => this.#handleClick(card));
+
+            img.onload = () => {
+                ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            }
+
+            img.src = url_image;
+            this.#container.appendChild(card);
+        }
+    }
+
+    get start(){
+        return this.#start;
+    }
+
+    deploy() {
+        this.#container.classList.add("game-container");
+        this.#container.id = "game-container";
+        this.#create();
+        this.#create();
+        document.body.prepend(this.#container);
+    }
+
+    
+
+    #handleClick(card) {
+        this.#click++;
+        this.#start = true
+        const cover = card.querySelector(".cover");
+        this.#unlock(cover);
+        this.#selected.push(card);
+
+        if (this.#selected.length === 2 && !this.#bothFund.includes(cover.dataset.id)) {
+            const [c1, c2] = this.#selected;
+            const id1 = c1.querySelector("canvas").dataset.id;
+            const id2 = c2.querySelector("canvas").dataset.id;
+
+            if (id1 !== id2) {
+                setTimeout(() => {
+                    this.#lock(c1.querySelector(".cover"));
+                    this.#lock(c2.querySelector(".cover"));
+                }, 400);
+                this.#selected = [];
+            } else {
+                this.#bothFund.push(id1);
+                this.#score += 5;
+                if (this.#bothFund.length === 8) {
+                    this.#isWinner = true;
+                }
+                this.#selected = [];
+            }
+        }
+
+    }
+
+    get isWinner() {
+        return this.#isWinner;
+    }
+
+    get click(){
+        return this.#click;
+    }
+
+    get score() {
+        return this.#score;
+    }
+
+    set score(score) {
+        this.#score = score;
+    }
+
+    get bothFund() {
+        return this.#bothFund;
+    }
+
+    reset(){
+        this.#bothFund = [];
+        this.#selected = [];
+        this.isWinner = false;
+        this.start = false;
+        this.score = 0;
+    }
+
+    #unlock(cov) {
+        cov.style.top = "-100px";
+        //cov.style.transition = ".6s";
+        cov.style.opacity = 0;
+        setTimeout(function () {
+            cov.style.display = "none"
+        }, 300)
+    }
+
+    #lock(cov) {
+        cov.style.display = "block"
+        setTimeout(function () {
+            cov.style.top = "0px";
+            cov.style.transition = ".5s";
+            cov.style.opacity = 1;
+        }, 300)
+    }
+}
+
+export { FundBothGame };
---
+++ style/js/script.js
@@ -0,0 +1,101 @@
+import { FundBothGame } from "./displayer.mjs";
+
+const screen = new FundBothGame();
+//Lancer le jeu
+screen.deploy();
+const divGame = document.querySelector("#game-container");
+const items = [...divGame.children];
+
+//Melanger le contenu 
+function mixedContent() {
+    for (let i = items.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i));
+        [items[i], items[j]] = [items[j], items[i]];
+    }
+
+    items.forEach(item => divGame.appendChild(item));
+}
+//Le panneau d'affichage score ... temps de jeu nombre d'interaction
+//Inialisation du dashboard
+const dashbord = document.createElement("div");
+dashbord.classList.add("dashbord");
+document.body.prepend(dashbord);
+//Creation du container pour le score et pour le nombre de click
+const scoreDisplay = document.createElement("p");
+const clickNumberDisplay = document.createElement("p");
+const timeDisplay = document.createElement("p");
+//Initialiser le score et le nombre de clique 
+scoreDisplay.innerHTML = messageScoreDisplay(screen.score);
+clickNumberDisplay.innerHTML = messageNumberClick(screen.click);
+timeDisplay.innerHTML = format(0, 0);
+//Ajouter le score au Dom
+dashbord.appendChild(scoreDisplay);
+dashbord.appendChild(clickNumberDisplay);
+dashbord.appendChild(timeDisplay);
+//Configuration de l'Interval
+let intervalId;
+
+divGame.addEventListener("click", (e) => {
+    if (e.target.dataset === e.currentTarget.dataset) {
+        e.preventDefault();
+    } else {
+        //Mise a jour du score et du nombre de clique
+        scoreDisplay.innerHTML = messageScoreDisplay(screen.score);
+        clickNumberDisplay.innerHTML = messageNumberClick(screen.click);
+        if (screen.isWinner) {
+            winner();
+        }
+        if (screen.click === 1) {
+            startConunter();
+        }
+
+    }
+});
+
+
+let time = 0;
+let seconde = 0;
+let minute = 0;
+function startConunter() {
+    intervalId ??= setInterval(function () {
+        time++;
+        minute = Math.floor(seconde / 60);
+        seconde = time - minute * 60;
+        timeDisplay.innerHTML = format(minute, seconde);
+    }, 1000);
+}
+
+function format(minute, second) {
+    return `${formatTime(minute)} : ${formatTime(second)}`
+}
+
+
+function formatTime(value) {
+    const val = value + '';
+    return val.length !== 2 ? '0' + val : val;
+}
+
+function messageScoreDisplay(value) {
+    return "Votre score :" + value;
+}
+
+function messageNumberClick(clickValue) {
+    return "Nombre de clique :" + clickValue;
+}
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    mixedContent();
+})
+
+function winner() {
+    alert(`Vous avez gagnée! en ${time} avec ${screen.click} clique  
+        \n votre score est de ${screen.score} \n Appuyer sur "Ok" pour réessayer
+        `);
+    screen.score = 0
+    scoreDisplay.innerHTML = messageScoreDisplay(screen.score);
+    clickNumberDisplay.innerHTML = messageNumberClick(screen.click);
+    mixedContent();
+    screen.reset();
+    clearInterval(intervalId);
+    intervalId = null;
+}
